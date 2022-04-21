7da34adc2ef18a8b9913e5c14133472b3b816e34
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-21 11:19:01 -0400

    GoRestFinal GorestfinalApplication.java

---
+++ src/main/java/com/example/gorestfinal/GorestfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.gorestfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorestfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorestfinalApplication.class, args);
+	}
+
+}
